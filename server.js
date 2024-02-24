c82eda99ae90e3392f0de1570e8cc125d9770640
JaCrawford92 <cckenny9@gmail.com>
2024-02-24 16:35:15 -0600

    More server setup

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
+const session = require('express-session')
 
 const app = express()
 const port = 3000
+
+// Middleware
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+// app.use(sessoion({
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
 
 app.get('/', (req, res) => {
     res.send('For the love of Coffee')
