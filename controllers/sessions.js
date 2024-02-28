097b5277f10d1defcb5ff9dc9828e35c3648f9b4
JaCrawford92 <cckenny9@gmail.com>
2024-02-27 19:48:46 -0600

    Sessions and User update

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,11 +1,37 @@
 const bcrypt = require('bcrypt')
 const router = require('express').Router()
 const User = require('../models/User.js')
+const { rawListeners } = require('../models/recipe.js')
 
 // GET /signup
 router.get('/new', (req, res) => {
     res.render('sessions/new.ejs', {
         currentUser: req.session.currentUser
+    })
+})
+
+// POST /sessions
+router.post('/', async (req, res) => {
+    try {
+        const foundUser = await User.findOne({username: req.body.username})
+        if(!foundUser){
+            res.send('Coffee user not found')
+        }else if(bcrypt.compareSync(req.body.password, foundUser.password)){
+            raq.session.currentUser = foundUser.username
+            res.redirect('/')
+        }else{
+            res.send('Invalid Coffee Password')
+        }
+
+    }catch(err){
+        console.log(err)
+    }
+})
+
+// DELETE /sessions
+router.delete('/', (req, res)=> {
+    req.session.destroy(()=>{
+        res.redirect('/')
     })
 })
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -6,4 +6,15 @@ const User = require('../models/User.js')
 router.get('/new', (req, res) => {
     res.render('users/new.ejs', {currentUser: req.session.currentUser})
 })
+
+// Create User
+router.post('/', async (req, res)=> {
+    try {
+        req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+        const newUser = await User.create(req.body)
+        res.redirect('/')
+    }catch(err){
+        console.log(err)
+    }
+})
 module.exports = router
